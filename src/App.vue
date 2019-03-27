a1ff59a5267ba3da08333240c640f1c880cab1d3
Armaan Gupta <guppythegod@tcff.co>
2019-03-27 09:33:29 -0500

    Addec vue-bootstrap

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url("https://use.typekit.net/zzi6vwu.css");
+
+#app {
+  font-family: proxima-nova;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+      <a href="https://www.guppy.im">
+        <h2>A <span class="bold">Guppy</span> Project</h2>
+      </a>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style scoped>
+
+h2 {
+  font-weight: 400;
+}
+
+a {
+  text-decoration: none;
+  color: #555555;
+}
+
+.hello {
+  text-align: center;
+  border-bottom: #555555 solid 1px;
+}
+
+.bold {
+  font-weight: 800;
+}
+
+</style>
